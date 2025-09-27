b8f303af6a1ad66c1d29be888039d13d44995e17
AZAD227 <harsh.singh.belwal278@gmail.com>
2025-09-27 11:58:11 +0530

    data transformation completed

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from network_security.components import data_transformation
 from network_security.components.data_ingestion import DataIngestion
+from network_security.components.data_validation import DataValidation
+from network_security.components.data_transformation import DataTransformation
 from network_security.entity.artifact_entity import DataIngestionArtifact
 from network_security.exception.exception import NetworkSecurityException
 from network_security.logging.logger import logging
-from network_security.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
+from network_security.entity.config_entity import DataIngestionConfig, DataValidationConfig, TrainingPipelineConfig, DataTransformationConfig
 
 import sys
 
@@ -15,6 +18,18 @@ if __name__=='__main__':
         dataingestionartifact=data_ingestion.initiate_data_ingestion()
         logging.info("Data Initiation Completed")
         print(dataingestionartifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
+        data_transformation_config=DataTransformationConfig(trainingpipelineconfig)
+        logging.info("data transformation started")
+        data_transformation=DataTransformation(data_validation_artifact,data_transformation_config)
+        data_transformation_artifact=data_transformation.initiate_data_transformation()
+        print(data_transformation_artifact)
+        logging.info("data transformation completed")
 
     except Exception as e:
         raise NetworkSecurityException(e,sys)
